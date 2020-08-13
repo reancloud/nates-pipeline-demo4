a781bb06816fd88db682b97a8664cf4a64264664
Nate Pan <Nate.Pan@hitachivantarafederal.com>
2020-08-13 11:42:51 -0400

    Added url test

--- config.rb
+++ config.rb
@@ -81,7 +81,7 @@ set :server_test_tool, :none
 #  - HCAP DevSecOps supports a simple syntax for declaring websites to be tested by HCAP Test.
 #  - HCAP DevSecOps will default to running all declared functional tests and load tests against
 #    all declared websites, unless you specifically declare which tests to run against which websites.
-website :mysite, protocol: :https, host: "www.example.com", path: ""
+website :mysite, protocol: :http, host: "ec2-15-200-113-249.us-gov-west-1.compute.amazonaws.com", path: ""
 
 # A list of HCAP Test functional tests to be run, with a minimal number of options required.
 #  - HCAP DevSecOps automatically applies default test execution options to each test in the list below,
